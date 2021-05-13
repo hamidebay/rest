4d35309288d0fe367dfb92d727d2a9f216a845ab
hamidebay <hamide.bay@baycom.ch>
2021-05-13 21:06:33 +0200

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,77 +1,61 @@
 import { v4 as uuidv4 } from 'uuid';
+import movieList from './data/movieList';
+import rents from './data/rents';
 var express = require("express");
 var router = express.Router();
 
-let movieList = [
-  {
-    id: "10002",
-    name: "die hard",
-    year: "1999",
-    genre: "action",
-    income: "10000000",
-  },
-  {
-    id: "10003",
-    name: "titanic",
-    year: "1997",
-    genre: "romantic",
-    income: "10000000",
-  },
-];
-
-
 /* GET home page. */
 router.get("/movies/", function (req, res, next) {
-/*   let newArray = JSON.parse(JSON.stringify(movieList));
-  let list = newArray.reverse().filter((o, i) => i < 20); */
+
   res.status(200).json(movieList);
 });
 
 router.get("/movies/:id", function (req, res, next) {
- /*  let newArray = JSON.parse(JSON.stringify(movieList));
-  let list = newArray.find((o, i) => i == req.params.id);
-  res.json(list);
-  console.log(list); */
+
   var id = req-prams.id;
+  res.status(200).json(id);
 });
 
 router.post("/movies/", function (req, res, next) {
-  movieList.push(req.body);
-  res.sendStatus(200);
+  let newMovie = req.body;
+  newMovie.id = uuidv4;
+  movieList.push(newMovie);
+  res.sendStatus(200).json(newMovie);
 });
 
 router.put("/movies/:id", function (req, res, next) {
-  /* let movies = req.movies;
-  movies = _.extend(movies, req.body); */
- /*  movies.save(function (err) {
-    if (err) {
-      return res.send("/movies/:id", {
-        errors: err.errors,
-        movies: movies,
-      });
-    } else {
-      res.jsonp(movies);
-    }
-  }); */
-  var id = req.params.id;
-  var getId = req.query.id;
-  var postId = req.body.id;
+  const editMovieId = req.params.id;
+  let editMovie = req.body;
+  let movie = data.find(movie=>movie.id === parseInt(editMovieId))
+  if(movie){
+     
+      res.status(200).send(editMovie);
+
+}
 });
 
 router.delete("movies/:id", function (req, res, next) {
-  //let deletMovies = movieList["movies" + req.params.id]
-  let newMovieList = JSON.stringify(movies, null, 1);
-  console.log(newMovieList);
+  const deletedMovieId = req.params.id;
+  const deletedMovie = data.find(movie=> movie.id === parseInt(deletedMovieId));
+  if(deletedMovie){
+  data = data.filter(movie => movie.id !==  parseInt(deletedMovieId))  
+  res.status(200).end();
+  }else{
+      res.status(404).json({errorMessage: "Film Yok"})
+  }
 });
 
-router.get("/rents", function (req, res, next) {});
+
+
+router.get("/rents", function (req, res, next) {
+  res.status(200).json(rents);
+});
 
 router.post("/rents", function (req, res, post) {
   let rentedMovie = req.body;
   uuidv4();
-  movieList.push(movieList);
-  res.status(200).jsonp(rentedMovie);
-  console.log(rents);
+  rents.push(rentedMovie);
+  res.status(200).json(rentedMovie);
 });
 
 ServiceWorkerRegistration.listen(3000, ()=>{
